f1e966daae990f8be476af133c4dd775c9855956
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-02-01 12:31:04 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from './gallery-items.js';
 
+const makeImageMarkup = image => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+ }
+
+const makeGallaryTable = galleryItems.map(makeImageMarkup).join("")
+
+const galleryEl = document.querySelector(".gallery")
+galleryEl.insertAdjacentHTML('afterbegin', makeGallaryTable)
+
+galleryEl.addEventListener("click", onSelectedImg)
+function onSelectedImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return
+  }
+  
+  console.log(e.target);
+  const bigImg = basicLightbox.create(
+    ` <img src="${e.target.dataset.source}"
+    width ="800"
+    height ="600" >`
+  )
+//   document.addEventListener("keydown", function (e)
+//   {
+//     if (e.key === "Escape") { 
+//       bigImg.onclose()
+//   } })
+ 
+  bigImg.show()
+  
+}
 console.log(galleryItems);
