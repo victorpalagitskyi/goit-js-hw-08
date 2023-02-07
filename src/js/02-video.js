fe1d49876c9939bd5ad461a8d7f2b6e09e30369b
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-02-07 12:17:14 +0200

    FIN

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 const key = "videoplayer-current-time"
 
-    player.on('timeupdate', ({seconds})=>localStorage.setItem(key, seconds))
+    player.on('timeupdate', throttle(({seconds})=>localStorage.setItem(key, seconds), 1000))
     player.setCurrentTime(localStorage.getItem(key) || 0);
